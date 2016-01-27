e891ad3b9bda08337332ef89c8e0fc1370fcbaf3
yijingping <jingping.yi@gmail.com>
2016-01-27 14:59:57 +0800

    add crontab requirements supervisord localsettings readme

--- unicrawler/settings.py
+++ unicrawler/settings.py
@@ -147,3 +147,14 @@ LOGGING = {
     },
 }
 
+
+## Import local settings
+try:
+    from local_settings import *
+except ImportError:
+    import sys, traceback
+    sys.stderr.write("Warning: Can't find the file 'local_settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("\nFor debugging purposes, the exception was:\n\n")
+    traceback.print_exc()
+
+
