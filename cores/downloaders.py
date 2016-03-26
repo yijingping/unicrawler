239ea4f34320ce39095a1c7adc6e39576a31f21d
yijingping <jingping.yi@gmail.com>
2016-03-26 16:26:37 +0800

    add selenium browser, django model processor backend, 抽象各个处理器

---
+++ cores/downloaders.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+__author__ = 'yijingping'
+import requests
+import platform
+from random import sample
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from django.conf import settings
+
+import logging
+logger = logging.getLogger()
+CRAWLER_CONFIG = settings.CRAWLER_CONFIG
+
+
+class RequestsDownloaderBackend(object):
+    """
+    使用requests直接访问
+    """
+    headers = [
+        {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'
+        }
+    ]
+
+    def __init__(self, proxy=None):
+        self.proxy = proxy
+
+    def format_proxies(self):
+        p = self.proxy
+        if self.proxy:
+            if p.user:
+                data = 'http://%s:%s@%s:%s' % (p.user, p.password, p.host, p.port)
+            else:
+                data = 'http://%s:%s' % (p.host, p.port)
+            return {
+                "http": data
+            }
+        else:
+            return None
+
+    def download(self, url):
+        header = sample(self.headers, 1)[0]
+        proxies = self.format_proxies()
+        if isinstance(url, basestring):
+            rsp = requests.get(url, headers=header, proxies=proxies)
+            rsp.close()
+            rsp.encoding = rsp.apparent_encoding
+            return rsp.text
+        elif isinstance(url, dict):
+            link, method, data, data_type = url.get('url'), url.get('method'), url.get('data'), url.get('dataType')
+            req = {'GET': requests.get, 'POST': requests.post}.get(method)
+            rsp = req(link, data=data, headers=header, proxies=proxies)
+            rsp.close()
+            rsp.encoding = rsp.apparent_encoding
+            if data_type == 'json':
+                return rsp.json()
+            else:
+                return rsp.text
+
+
+class SeleniumDownloaderBackend(object):
+    """
+    使用Selenium模拟浏览器访问
+    """
+    headers = [
+        {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'
+        }
+    ]
+
+    def __init__(self, proxy=None):
+        # 设置代理
+        self.proxy = proxy
+
+    def __enter__(self):
+        # 打开界面
+        self.display = self.get_display()
+        #  打开浏览器
+        self.browser = self.get_browser(self.proxy)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # 关闭浏览器
+        try:
+            if self.browser:
+                self.browser.delete_all_cookies()
+                self.browser.quit()
+        except Exception as e:
+            logging.exception(e)
+        # 关闭界面
+        try:
+            # 关闭浏览器,关闭窗口
+            self.display and self.display.stop()
+        except Exception as e:
+            logging.exception(e)
+
+    def get_display(self):
+        if platform.system() != 'Darwin':
+            # 不是mac系统, 启动窗口
+            display = Display(visible=0, size=(1024, 768))
+            display.start()
+        else:
+            display = None
+        return display
+
+    def get_browser(self, proxy):
+        # 启动浏览器
+        # 禁止加载image
+        firefox_profile = webdriver.FirefoxProfile()
+        #firefox_profile.set_preference('permissions.default.stylesheet', 2)
+        #firefox_profile.set_preference('permissions.default.image', 2)
+        #firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
+        # 代理
+        if proxy.is_valid():
+            myProxy = '%s:%s' % (proxy.host, proxy.port)
+            ff_proxy = Proxy({
+                'proxyType': ProxyType.MANUAL,
+                'httpProxy': myProxy,
+                'ftpProxy': myProxy,
+                'sslProxy': myProxy,
+            'noProxy':''})
+
+            browser = webdriver.Firefox(firefox_profile=firefox_profile, proxy=ff_proxy)
+        else:
+            browser = webdriver.Firefox(firefox_profile=firefox_profile)
+
+        return browser
+
+    def download(self, url):
+        browser = self.browser
+        # 访问首页, 输入wchatid, 点击查询
+        browser.get(url)
+        js = """
+            return document.documentElement.innerHTML;
+        """
+        body = browser.execute_script(js)
+        return body
+
+
+class BrowserDownloaderBackend(object):
+    def download(self):
+        pass
---
+++ cores/processors.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+__author__ = 'yijingping'
+from abc import ABCMeta
+from abc import abstractmethod
+
+import _mysql
+import torndb
+from datetime import datetime
+from django.utils.encoding import smart_str, smart_unicode
+from django.db import models
+from cores.util import get_uniqueid
+import logging
+logger = logging.getLogger()
+
+
+class BaseProcessorBackend(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def process(self, data):
+        pass
+
+
+class MysqlBackend(BaseProcessorBackend):
+    @property
+    def _table(self):
+        return self.db_table
+
+    def __init__(self, config):
+        db_config = config['database']
+        self.db = torndb.Connection(
+            host=db_config.get("host"),
+            database=db_config.get("name"),
+            user=db_config.get("user"),
+            password=db_config.get("password"),
+            charset=db_config.get("charset")
+        )
+        self.db_table = config['table']
+        self.defaults = config['defaults']
+        self.unique_key = config["unique_key"]
+
+    def process(self, params, filters=None):
+        # 加上默认值
+        data = params.copy()
+        for k, v in self.defaults.iteritems():
+            data.setdefault(k, v)
+
+        # 设置唯一键
+        unique_value = ':'.join(['%s' % data[k] for k in self.unique_key])
+        data['uniqueid'] = get_uniqueid(unique_value)
+        data['update_time'] = str(datetime.now())
+        # 清除数据
+        data.pop('seed_id', None)
+        data.pop('rule_id', None)
+        data.pop('detail_multi', None)
+        # 更新或插入数据库
+        try:
+            # try update
+            affected = self.update(data, {'uniqueid': data['uniqueid']})
+            if affected == 0:
+                # row not exists, try create
+                data['create_time'] = str(datetime.now())
+                self.create(data)
+        except Exception as e:
+            logger.exception(e)
+        finally:
+            logger.debug(data['url'])
+
+    def create(self, params):
+        keys = params.keys()
+        values = params.values()
+        cols = ','.join(map(lambda s:str(s), keys))
+        placeholder = ','.join(['%s' for _ in range(len(keys))])
+        sql = 'INSERT INTO ' + self._table + ' (' + cols + ') ' + ' VALUES (' + placeholder + ');'
+
+        return self.db.insert(sql, *values)
+
+    def update(self, params, filters=None):
+        set_keys = params.keys()
+        values = params.values()
+        set_placeholder = ', '.join(['`'+item+'`=%s' for item in set_keys])
+        sql = 'UPDATE ' + self._table + ' SET ' + set_placeholder
+        if filters:
+            where_keys = filters.keys()
+            where_values = filters.values()
+            where_placeholder = ', '.join(['`'+item+'`=%s' for item in where_keys])
+            sql = sql + ' WHERE ' + where_placeholder
+            values += where_values
+        return self.db.update(sql, *values)
+
+    @staticmethod
+    def dict_to_sql(params, sep=', '):
+        cols = []
+        for k, v in params.iteritems():
+            k2 = _mysql.escape_string(str(k))
+            if v is None:
+                col = '`%s`=NULL' % k2
+            elif isinstance(v, (int, long, float)):
+                col = '`%s`=%s' % (k2, v)
+            elif isinstance(v, unicode):
+                v2 =  v.encode('utf-8')
+                col = '`%s`="%s"' % (k2, smart_unicode(_mysql.escape_string(smart_str(v))))
+            else:
+                col = '`%s`="%s"' % (k2, v)
+            cols.append(col)
+        return smart_unicode(sep.join(cols))
+
+    @staticmethod
+    def fields_to_sql(fields):
+        f2 = ["`%s`" % item if item != "*" else "*" for item in fields]
+        return _mysql.escape_string(', '.join(f2))
+
+
+class DjangoModelBackend(BaseProcessorBackend):
+    def __init__(self, config):
+        self.defaults = config['defaults']
+        self.unique_key = config["unique_key"]
+        modelstr = config["DjangoModel"]
+        modelclass = models.get_model(modelstr.split('.')[0], modelstr.split('.')[-1])
+        self._class = modelclass
+
+    def process(self, params):
+        C = self._class
+        params['uniqueid'] = get_uniqueid('%s:%s' % (params['wechat_id'], params['title']))
+        C.objects.update_or_create(uniqueid=params['uniqueid'], defaults=params)
+
+        # 加上默认值
+        data = params.copy()
+        for k, v in self.defaults.iteritems():
+            data.setdefault(k, v)
+
+        # 设置唯一键
+        unique_value = ':'.join(['%s' % data[k] for k in self.unique_key])
+        data['uniqueid'] = get_uniqueid(unique_value)
+        data['update_time'] = str(datetime.now())
+        # 清除数据
+        data.pop('seed_id', None)
+        data.pop('rule_id', None)
+        data.pop('detail_multi', None)
+        # 更新或插入数据库
+        try:
+            C.objects.update_or_create(uniqueid=data['uniqueid'], defaults=data)
+        except Exception as e:
+            logger.exception(e)
+        finally:
+            logger.debug(data['url'])
+
+
+class MongoDBBackend(BaseProcessorBackend):
+    pass
